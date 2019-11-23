7d0f2d8d7d863709bf83ebcc973b63fb70711c41
Karl Brown <kabrown+va@thoughtworks.com>
2019-11-22 19:54:30 -0500

    Update sort approach in clean.sh to be a bit more stable and work on MacOSX, update acronyms.csv according to new sort approach

--- clean.sh
+++ clean.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-sort -u acronyms.csv | uniq -i > output.csv
-sed -i '/Title,/d' output.csv
-sed -i '1iTitle, Meaning, Context, Notes' output.csv
+sort -d -f acronyms.csv | uniq -i > output.csv
+sed -i '' '/Title,/d' output.csv
+sed -i '' '1i\
+Title,Meaning,Context,Notes
+' output.csv
