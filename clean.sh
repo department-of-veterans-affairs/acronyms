0b2476e74bce9b7fdfe727b43024b918baef744c
Karl Brown <kabrown+va@thoughtworks.com>
2019-11-25 11:21:24 -0500

    Added a lot of acronyms, cleaned up file a bit (#25)
    * Update sort approach in clean.sh to be a bit more stable and work on MacOSX, update acronyms.csv according to new sort approach
    
    * Added a bunch of acronyms from excel sheet found on VA internal website
    
    * Update clean script to call csvlint if it exists
    
    * Fix a bunch of typos and removed some duplicates
    
    * A bit more deduping
    
    * Add a few more acronyms

--- clean.sh
+++ clean.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
-sort -u acronyms.csv | uniq -i > output.csv
-sed -i '/Title,/d' output.csv
-sed -i '1iTitle, Meaning, Context, Notes' output.csv
+sort -d -f acronyms.csv | uniq -i > output.csv
+sed -i '' '/Title,/d' output.csv
+sed -i '' '1i\
+Title,Meaning,Context,Notes
+' output.csv
+
+# run csv lint if installed (https://github.com/theodi/csvlint.rb)
+if [ -x "$(command -v csvlint)" ]; then
+  csvlint output.csv
+fi
