d057777db27010d41facc70d0b026543c2b33c7c
Karl Brown <kabrown+va@thoughtworks.com>
2019-12-04 00:49:57 -0500

    Add some scripts to help detect duplicates and errors

---
+++ print-dupe-acronyms.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Prints out duplicate acronyms (first column matches)
+
+awk -F , 'NR==FNR{a[$1]++; next} a[$1]>1' acronyms.csv acronyms.csv | sort
---
+++ print-dupe-definitions.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Prints duplicate definitions from the acronyms file
+# Note: This isn't fully correct because it doesn't handle quoted strings with commas correctly
+awk -F , 'NR==FNR{a[$2]++; next} a[$2]>1' acronyms.csv acronyms.csv | sort -k 2
