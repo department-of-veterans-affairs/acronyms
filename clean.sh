2e9662944c9809dc0356aa68b7c080a80461b701
keithdadkins <keithdadkins@me.com>
2020-01-07 16:26:32 -0600

    refactor clean.sh to be more portable
    Using sed with `-i ''` is not portable across *nixes. For example,
    GNU sed chokes on `-i ''`. This patch uses grep and cat instead.

--- clean.sh
+++ clean.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
+# remove dups (ignores case)
 sort -d -f acronyms.csv | uniq -i > output.csv
-sed -i '' '/Title,/d' output.csv
-sed -i '' '1i\
-Title,Meaning,Context,Notes
-' output.csv
+
+# delete csv header row.
+grep -v '^Title\,Meaning\,Context\,Notes' acronyms.csv > output.csv
+
+# add back header row
+echo 'Title,Meaning,Context,Notes' | cat - output.csv > temp && mv temp output.csv
 
 # run csv lint if installed (https://github.com/theodi/csvlint.rb)
 if [ -x "$(command -v csvlint)" ]; then
