4e51f7f67efc45d06c14572444caee4cc7ec2ef7
Karl Brown <kabrown+va@thoughtworks.com>
2024-06-21 14:17:35 -0400

    Moves to use poetry and ruff, bumps to latest versions of dependencies

--- tests/test_format_acronyms.py
+++ tests/test_format_acronyms.py
@@ -1,7 +1,7 @@
-from format_acronyms import fix_smart_quotes, fix_all_upper, fix_all_lower
+from format_acronyms.format_acronyms import fix_all_lower, fix_all_upper, fix_smart_quotes
 
 SMARTQUOTE_LINE = ["“smartdouble”", "‘smartsingle’"]
-SMART_QUOTE_LINE_FIXED = ["\"smartdouble\"", "\'smartsingle\'"]
+SMART_QUOTE_LINE_FIXED = ['"smartdouble"', "'smartsingle'"]
 ALL_UPPER_LINE = ["WSDL", "WEB SERVICES DESCRIPTION LANGUAGE"]
 ALL_UPPER_LINE_FIXED = ["WSDL", "Web Services Description Language"]
 ALL_LOWER_LINE = ["WSDL", "web services description language"]
